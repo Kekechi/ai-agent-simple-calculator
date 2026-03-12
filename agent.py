137bc38207b01ef99b1d56576eca0c047313e2a0
Keigo <56617711+Kekechi@users.noreply.github.com>
2026-03-11 17:55:42 -0700

    Simple agent call

--- agent.py
+++ agent.py
@@ -1,1 +1,44 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os, json, tools
 
+load_dotenv('.env')
+API_KEY = os.getenv('API_KEY')
+API_URL = os.getenv('API_URL')
+
+MAX_ITERATION = 5
+
+class Agent:
+  def __init__(self):
+    self.client = OpenAI(
+      api_key=API_KEY,
+      base_url=API_URL
+    )
+
+    self.messages = []
+
+    self.messages.append({
+      "role":"system",
+      "content":"""You are a helpful AI assistant with access to simple calculation tools.
+      Follow these rules:
+      1. When asked to calculate math equations, always use the available tools first
+      2. If a tool returns an error, explain the error to the user clearly"""
+    })
+
+  def process_query(self,user_input):
+    self.messages.append({
+      "role":"user",
+      "content":user_input
+    })
+
+    completion = self.client.chat.completions.create(
+      model="gpt-5-nano",
+      messages=self.messages
+    )
+
+    response_message = completion.choices[0].message
+    print(response_message)
+    return response_message
+
+    # try:
+    #   for _ in range(MAX_ITERATION):
